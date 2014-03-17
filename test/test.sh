5612e00c75270d7c2572764212d33eb5b428636e
clowwindy <clowwindy42@gmail.com>
2014-03-17 23:55:11 +0800

    improve test

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pushd .. && \
-python gfwlist2pac/main.py  -i test/gfwlist.txt -f proxy.pac -p 'SOCKS5 127.0.0.1:1080' && \
+python gfwlist2pac/main.py  -i test/gfwlist.txt -f test/proxy.pac -p 'SOCKS5 127.0.0.1:1080' && \
 popd && \
-cat ../proxy.pac test_case.js > /tmp/test.js && \
+cat proxy.pac && \
+cat proxy.pac test_case.js > /tmp/test.js && \
 node /tmp/test.js && \
 echo 'Test passed'
