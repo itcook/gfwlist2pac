f651a3003df0c8771804036e22ffb2380c60dac8
clowwindy <clowwindy42@gmail.com>
2014-03-18 22:15:21 +0800

    add performance test

--- test/performance_test.sh
+++ test/performance_test.sh
@@ -3,7 +3,6 @@
 pushd .. && \
 PYTHONPATH=. python gfwlist2pac/main.py  -i test/gfwlist.txt -f test/proxy.pac -p 'SOCKS5 127.0.0.1:1080' --user-rule test/user_rule.txt && \
 popd && \
-cat proxy.pac && \
 cat proxy.pac performance_test.js > /tmp/test.js && \
 node /tmp/test.js && \
 echo 'Test passed'
