1a4296e1e0abeac09fd963c07cb7633638a950f8
clowwindy <clowwindy42@gmail.com>
2014-06-13 15:55:26 +0800

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name="gfwlist2pac",
-    version="1.0.5",
+    version="1.0.6",
     license='MIT',
     description="convert gfwlist2pac to pac",
     author='clowwindy',
